f17e79aa671adb08ce9949aecae8138d4c1051a2
李星星 <sss60@qq.com>
2019-06-18 10:26:37 +0800

    update

---
+++ src/Commands/SwooleServer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SwooleServer\Commands\Commands;
+use Illuminate\Console\Command;
+
+class SwooleServer extends Command
+{
+    protected $signature = 'command:SwooleServer {type : start|stop|restart}}';
+
+    public function handle()
+    {
+
+    }
+}
---
+++ src/LaravelServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+use Illuminate\Support\ServiceProvider;
+
+class HttpServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+}
