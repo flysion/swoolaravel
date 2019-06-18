7a1b7c31c3e91f34e23de9edcc074bb9b4b95e9f
李星星 <sss60@qq.com>
2019-06-18 13:53:32 +0800

    update

--- src/Commands/Service.php
+++ src/Commands/Service.php
@@ -5,9 +5,9 @@ use Illuminate\Console\Command;
 
 class Service extends Command
 {
-    protected $signature = 'swoole_server:service {type}}';
+    protected $signature = 'laravoole:service {type}}';
 
-    protected $description = 'swoole_server的服务管理';
+    protected $description = '服务管理';
 
     public function handle()
     {
