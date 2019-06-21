98d0c41775fb86bc985d4cef92d00807c0ecc8b3
李星星 <sss60@qq.com>
2019-06-21 13:31:30 +0800

    update

--- config/webserver.php
+++ config/webserver.php
@@ -4,9 +4,9 @@ return [
 
     'host' => env('LARAVOOLE_HOST', '0.0.0.0'),
     'port' => env('LARAVOOLE_PORT', '9999'),
-
     // see https://wiki.swoole.com/wiki/page/353.html
     'process_mode' => SWOOLE_PROCESS,
+    'sock_type' => SWOOLE_SOCK_TCP,
 
     // see https://wiki.swoole.com/wiki/page/620.html
     'server_options' => [
--- routes/webserver.php
+++ routes/webserver.php
@@ -1,2 +1,1 @@
 <?php
-echo get_class($this);
