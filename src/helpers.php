e81249ccb8079c3715d403f3a5f0b57700a4aa02
李星星 <sss60@qq.com>
2021-05-11 15:51:53 +0800

    update

--- src/helpers.php
+++ src/helpers.php
@@ -3,6 +3,7 @@
 namespace Flysion\Swoolaravel;
 
 use Symfony\Component\HttpFoundation\HeaderBag;
+use Symfony\Component\HttpFoundation\ServerBag;
 
 const events = [
     'start' => Events\Start::class,
@@ -36,25 +37,41 @@ const events = [
  */
 function swoole_request_to_laravel_request(\Swoole\Http\Request $request) : \Illuminate\Http\Request
 {
+    $server = [];
+
     foreach($request->server as $k => $v)
     {
         $k = strtoupper($k);
         $server[$k] = $v;
     }
 
-    $req = \Illuminate\Http\Request::create(
-        $request->server['path_info'] . (@$request->server['query_string'] ? '?' . $request->server['query_string'] : ''),
-        $request->server['request_method'],
-        $request->post ?: [],
-        $request->cookie ?: [],
-        $request->files ?: [],
-        $server,
-        $request->rawContent()
+    foreach($request->header as $k => $v) {
+        $k = 'HTTP_' . strtoupper(str_replace('-', '_', $k));
+        $server[$k] = $v;
+    }
+
+    if($server['HTTP_CONTENT_TYPE']) {
+        $server['CONTENT_TYPE'] = $server['HTTP_CONTENT_TYPE'];
+    }
+
+    $uri = $request->server['path_info'];
+    if($request->get) {
+        $uri .= '?' . http_build_query($request->get);
+    }
+
+    $swooleRequest = \Illuminate\Http\Request::createFromBase(
+        \Illuminate\Http\Request::create(
+            $uri,
+            $request->server['request_method'],
+            $request->post ?: [],
+            $request->cookie ?: [],
+            $request->files ?: [],
+            $server,
+            $request->rawContent()
+        )
     );
 
-    $req->headers = new HeaderBag($request->header);
-
-    return $req;
+    return $swooleRequest;
 }
 
 /**
